c2567bd6a870115a839177ded283b3eb0396a1bb
v_maqinglong <v_maqinglong@baidu.com>
2020-07-07 20:20:52 +0800

    Initial commit

---
+++ app/src/main/java/com/malong/downloadsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.malong.downloadsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
